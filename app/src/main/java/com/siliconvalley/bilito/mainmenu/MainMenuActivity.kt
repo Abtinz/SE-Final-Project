ff35cae170b0424e03bc37683de3a6921bd1fabd
Abtinz <abtinzandi@gmail.com>
2023-01-17 22:08:26 +0330

    code cleaning

---
+++ app/src/main/java/com/siliconvalley/bilito/mainmenu/MainMenuActivity.kt
@@ -0,0 +1,21 @@
+package com.siliconvalley.bilito.mainmenu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+
+
+class MainMenuActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+
+}
