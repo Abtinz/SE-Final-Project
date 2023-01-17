2c91fa5045d9d103b3c37a590c13c406cf6beac0
Abtinz <abtinzandi@gmail.com>
2023-01-17 22:13:59 +0330

    this class will implement our CoroutineScope with our Fragment

---
+++ app/src/main/java/com/siliconvalley/bilito/commonServices/coroutineBased/BaseFragmentCoroutineClass.kt
@@ -0,0 +1,30 @@
+package com.siliconvalley.bilito.commonServices.coroutineBased
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
+
+/*
+ * this class will implement our CoroutineScope with our Fragment
+ */
+abstract class BaseFragmentCoroutineClass:Fragment(),CoroutineScope {
+
+    private lateinit var job : Job
+
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job = Job()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
