61cfaf5ce47d26173294fd0edf8a6d0adbefd701
Abtinz <abtinzandi@gmail.com>
2023-01-17 22:40:50 +0330

    our data base mange which will get interface of user data base model and Dao and will implement service of data base

---
+++ app/src/main/java/com/siliconvalley/bilito/profile/db/user/UserDataBase.kt
@@ -0,0 +1,41 @@
+package com.siliconvalley.bilito.profile.db.user
+
+import androidx.room.Database
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+/*
+ * our data base mange which will get interface of user data base model and Dao and will implement service of data base
+ */
+@Database(
+    entities = [User::class],
+    version = 1 ,
+    exportSchema = false
+)
+
+abstract class UserDataBase:RoomDatabase() {
+    abstract fun getUserDao(): UserDao
+
+    /*companion object{
+        //Volatile is for immidiate  /
+        @Volatile private var instance : UserDataBase? = null/
+
+        private val LOCK = Any()
+
+        private fun dbBuilder(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            UserDataBase::class.java,
+            "userDB"
+        ).allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build()
+
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: dbBuilder(context).also {
+                instance = it
+            }
+        }
+    }*/
+
+}
