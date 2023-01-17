7688ebc4ad208087c7572a598061942199276b31
Abtinz <abtinzandi@gmail.com>
2023-01-17 22:23:40 +0330

    just commit

--- app/src/main/java/com/siliconvalley/bilito/commonServices/network/api/client/BaseRetrofitClientService.kt
+++ app/src/main/java/com/siliconvalley/bilito/commonServices/network/api/client/BaseRetrofitClientService.kt
@@ -4,6 +4,8 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 
+//this class will help us to have header via token passing
+//header title like Authorization and url for basic url of api
 class BaseRetrofitClientService(headerTitle: String , headerText:String , url :String) {
     lateinit var retrofit: Retrofit
     init{
@@ -11,5 +13,7 @@ class BaseRetrofitClientService(headerTitle: String , headerText:String , url :S
         val moshi = Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
+
+
     }
 }
