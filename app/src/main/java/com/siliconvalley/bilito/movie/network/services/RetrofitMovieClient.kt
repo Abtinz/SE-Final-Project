315a18230ea55e8b3cd6a8854e03cd40f5af1ca1
Abtinz <abtinzandi@gmail.com>
2023-01-18 00:12:10 +0330

    movie new retrofit service and debugging cinema retrofit

--- app/src/main/java/com/siliconvalley/bilito/movie/network/services/RetrofitMovieClient.kt
+++ app/src/main/java/com/siliconvalley/bilito/movie/network/services/RetrofitMovieClient.kt
@@ -1,13 +1,13 @@
-package com.siliconvalley.bilito.cinemalist.network.api.service
+package com.siliconvalley.bilito.movie.network.services
 
-import com.siliconvalley.bilito.commonServices.network.api.utils.ApiUtils.API_BASE_URL
+import com.siliconvalley.bilito.cinemalist.network.api.service.CinemaApiService
+import com.siliconvalley.bilito.commonServices.network.api.utils.ApiUtils
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-object RetrofitCinemaClient {
-
+object RetrofitMovieClient {
     //moshi converter
     private val moshi =  Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
@@ -15,11 +15,11 @@ object RetrofitCinemaClient {
 
     //retrofit service
     private val retrofit = Retrofit.Builder()
-        .baseUrl(API_BASE_URL)
+        .baseUrl(ApiUtils.API_BASE_URL)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
 
-    val cinemaApiService : CinemaApiService by lazy {
-        retrofit.create(CinemaApiService::class.java)
+    val movieApiService : MovieApiServices by lazy {
+        retrofit.create(MovieApiServices::class.java)
     }
 }
