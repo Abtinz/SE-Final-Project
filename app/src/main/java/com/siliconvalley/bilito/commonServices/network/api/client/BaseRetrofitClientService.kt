00fa3ebbe5a59c77b17b3d43a3faa0128f713a5a
Abtinz <abtinzandi@gmail.com>
2023-01-17 22:25:20 +0330

    okHttpClient -> this section will make a client for our api which can have a header for our tokens

--- app/src/main/java/com/siliconvalley/bilito/commonServices/network/api/client/BaseRetrofitClientService.kt
+++ app/src/main/java/com/siliconvalley/bilito/commonServices/network/api/client/BaseRetrofitClientService.kt
@@ -2,6 +2,7 @@ package com.siliconvalley.bilito.commonServices.network.api.client
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
 //this class will help us to have header via token passing
@@ -14,6 +15,14 @@ class BaseRetrofitClientService(headerTitle: String , headerText:String , url :S
             .add(KotlinJsonAdapterFactory())
             .build()
 
-
+        // this section will make a client for our api which can have a header for our tokens
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val originalRequest = chain.request()
+                val requestBuilder = originalRequest.newBuilder()
+                    .addHeader(headerTitle, headerText)
+                    .method(originalRequest.method,originalRequest.body)
+                chain.proceed(requestBuilder.build())
+            }.build()
     }
 }
