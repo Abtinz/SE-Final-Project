e72901728b3214ef3394e7c0da6e169a8bfdd2a0
Abtinz <abtinzandi@gmail.com>
2023-01-19 17:11:55 +0330

    all data bas and api services are implemented for main menu

---
+++ app/src/main/java/com/siliconvalley/bilito/mainmenu/ui/viewModel/MainMenuViewModel.kt
@@ -0,0 +1,57 @@
+package com.siliconvalley.bilito.mainmenu.ui.viewModel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.siliconvalley.bilito.cinemalist.network.api.responses.cinema.Cinema
+import com.siliconvalley.bilito.mainmenu.network.api.RetrofitMainMenuService
+import com.siliconvalley.bilito.movie.db.MoviesDataBase
+import com.siliconvalley.bilito.movie.db.MoviesEntity
+import com.siliconvalley.bilito.movie.network.responses.movie.Movie
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.forEach
+import kotlinx.coroutines.launch
+
+class MainMenuViewModel:ViewModel() {
+    private val _latestMovieList  = MutableStateFlow(emptyList<Movie>())
+    val latestMovieList: StateFlow<List<Movie>>
+        get() = _latestMovieList
+
+    private val _bestMovieList  = MutableStateFlow(emptyList<Movie>())
+    val bestMovieList: StateFlow<List<Movie>>
+        get() = _bestMovieList
+
+    private val _cinemaList  = MutableStateFlow(emptyList<Cinema>())
+    val cinemaList: StateFlow<List<Cinema>>
+        get() = _cinemaList
+
+    fun lastestMovieApiDataBase(context: Context){
+        viewModelScope.launch {
+            val dataBaseManager = MoviesDataBase(context).getMovieDao()
+            val retrofitService = RetrofitMainMenuService.mainMenuApiService
+            _latestMovieList.value = retrofitService.latestMovie()
+            _latestMovieList.value.forEach {movie ->
+                dataBaseManager.newMovie(MoviesEntity(movie))
+            }
+        }
+    }
+
+    fun bestMovieApiDataBase(context: Context){
+        viewModelScope.launch {
+            val dataBaseManager = MoviesDataBase(context).getMovieDao()
+            val retrofitService = RetrofitMainMenuService.mainMenuApiService
+            _bestMovieList.value = retrofitService.bestMovie()
+            _bestMovieList.value.forEach {movie ->
+                dataBaseManager.newMovie(MoviesEntity(movie))
+            }
+        }
+    }
+
+    fun cinemaListApiDataBase(){
+        viewModelScope.launch {
+            val retrofitService = RetrofitMainMenuService.mainMenuApiService
+            _cinemaList.value = retrofitService.cinemaLists()
+        }
+    }
+}
