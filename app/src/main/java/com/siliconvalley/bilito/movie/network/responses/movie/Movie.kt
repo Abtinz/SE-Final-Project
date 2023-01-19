2b71f510ed80e6cfa12649e79ab233ade572f11c
Abtinz <abtinzandi@gmail.com>
2023-01-19 17:25:32 +0330

    refactoring!

--- app/src/main/java/com/siliconvalley/bilito/movie/network/responses/movie/Movie.kt
+++ app/src/main/java/com/siliconvalley/bilito/movie/network/responses/movie/Movie.kt
@@ -4,14 +4,14 @@ import com.siliconvalley.bilito.profile.network.api.responses.comments.Comments
 
 data class Movie(
     val id:String,
-    val title : String,
+    val name : String,
     val length : String,
     val picture : String,
     val director : List<String>,
     //val director : List<Director>
     val actors : List<String>,
     //val actors : List<Actors>
-    val comments : List<Comments>,
+    val reviews : List<Comments>,
 ) {
 
 }
