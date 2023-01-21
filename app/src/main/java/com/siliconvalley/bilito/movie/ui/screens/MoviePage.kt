14a5fe929bb7e18ae1fde077c248b02f5ca51e0b
Abtinz <abtinzandi@gmail.com>
2023-01-21 21:35:22 +0330

    view model initiate and reload view

--- app/src/main/java/com/siliconvalley/bilito/movie/ui/screens/MoviePage.kt
+++ app/src/main/java/com/siliconvalley/bilito/movie/ui/screens/MoviePage.kt
@@ -1,9 +1,27 @@
 package com.siliconvalley.bilito.movie.ui.screens
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.siliconvalley.bilito.commonServices.ui.compose.reload.ReloadView
+import com.siliconvalley.bilito.movie.ui.viewMdel.MovieViewModel
 
 @Composable
 fun MoviePage(navController: NavController,movieId:String) {
+    val viewModel = viewModel(MovieViewModel::class.java)
+    val context = LocalContext.current
+    val movieSimpleData by viewModel.movieSimpleInfo.collectAsState()
+    viewModel.api(movieId , context)
+    LazyColumn{
+        if(movieSimpleData.isEmpty()){
+            item{
+                    ReloadView()
+            }
+        }
+    }
 
 }
