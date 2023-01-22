02a5ecc319bfbb44b43e0d531e56817a1f750cfe
SarvinNami <srvn0nm@gmail.com>
2023-01-22 18:22:02 +0330

    designing the base of the profile xml

--- app/src/main/java/com/siliconvalley/bilito/profile/ui/activity/ProfileActivity.kt
+++ app/src/main/java/com/siliconvalley/bilito/profile/ui/activity/ProfileActivity.kt
@@ -2,11 +2,17 @@ package com.siliconvalley.bilito.profile.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.siliconvalley.bilito.R
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+    }
+
+    fun imageEditor(view: View){
 
     }
 }
