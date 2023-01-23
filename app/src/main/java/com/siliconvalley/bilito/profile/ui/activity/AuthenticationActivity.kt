888da2e4b525f2f75fd26192f608a67940b1505e
SarvinNami <srvn0nm@gmail.com>
2023-01-23 19:00:48 +0330

    adding a signup button and finishing the design of the login cardview

--- app/src/main/java/com/siliconvalley/bilito/profile/ui/activity/AuthenticationActivity.kt
+++ app/src/main/java/com/siliconvalley/bilito/profile/ui/activity/AuthenticationActivity.kt
@@ -2,11 +2,16 @@ package com.siliconvalley.bilito.profile.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.siliconvalley.bilito.R
 
 class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+    }
+
+    fun loginButton(view: View){
+
     }
 }
