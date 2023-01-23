7d89627bdcd7ceeae510dbfca64698c2c0dba2ab
SarvinNami <srvn0nm@gmail.com>
2023-01-23 19:32:27 +0330

    writing the little signup button in the login cardview

--- app/src/main/java/com/siliconvalley/bilito/profile/ui/activity/AuthenticationActivity.kt
+++ app/src/main/java/com/siliconvalley/bilito/profile/ui/activity/AuthenticationActivity.kt
@@ -1,8 +1,11 @@
 package com.siliconvalley.bilito.profile.ui.activity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.motion.widget.Key.VISIBILITY
 import com.siliconvalley.bilito.R
 
 class AuthenticationActivity : AppCompatActivity() {
@@ -15,7 +18,11 @@ class AuthenticationActivity : AppCompatActivity() {
 
     }
 
+    @SuppressLint("ResourceType")
     fun signUpButton(view: View){
-
+        val loginCardView = findViewById<CardView>(R.id.loginCardView)
+        val signUpCardView = findViewById<CardView>(R.id.signUpCardView)
+        loginCardView.visibility = View.GONE
+        signUpCardView.visibility = View.VISIBLE
     }
 }
