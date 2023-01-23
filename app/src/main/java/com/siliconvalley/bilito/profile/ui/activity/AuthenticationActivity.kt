7c4649ec1d908eff3996b50783d4debdf9fe9cf9
SarvinNami <srvn0nm@gmail.com>
2023-01-23 18:19:54 +0330

    adding a new activity for signin and signup

---
+++ app/src/main/java/com/siliconvalley/bilito/profile/ui/activity/AuthenticationActivity.kt
@@ -0,0 +1,11 @@
+package com.siliconvalley.bilito.profile.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
