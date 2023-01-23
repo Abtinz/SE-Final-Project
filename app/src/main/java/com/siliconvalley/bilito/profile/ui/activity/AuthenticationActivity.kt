47ed6f72c7e3dfb8cae05d6ffb466a0cbf040d65
SarvinNami <srvn0nm@gmail.com>
2023-01-23 20:19:04 +0330

    adding photo from gallery

--- app/src/main/java/com/siliconvalley/bilito/profile/ui/activity/AuthenticationActivity.kt
+++ app/src/main/java/com/siliconvalley/bilito/profile/ui/activity/AuthenticationActivity.kt
@@ -1,14 +1,20 @@
 package com.siliconvalley.bilito.profile.ui.activity
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageView
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.motion.widget.Key.VISIBILITY
 import com.siliconvalley.bilito.R
 
 class AuthenticationActivity : AppCompatActivity() {
+    var imageUri: Uri? = null
+    val imageProfileView = findViewById<ImageView>(R.id.imageProfileView)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
@@ -24,5 +30,18 @@ class AuthenticationActivity : AppCompatActivity() {
         val signUpCardView = findViewById<CardView>(R.id.signUpCardView)
         loginCardView.visibility = View.GONE
         signUpCardView.visibility = View.VISIBLE
+    }
+
+    fun imageEditor(view: View){
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        startActivityForResult(gallery, 100)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == 100){
+            imageUri = data?.data
+            imageProfileView.setImageURI(imageUri)
+        }
     }
 }
